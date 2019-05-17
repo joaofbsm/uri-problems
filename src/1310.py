25d5f11cbc3ecc4edb3746dbabca0ee5e39e6ee5
João Francisco <joaofbsm@hotmail.com>
2019-05-17 02:03:27 -0300

    Add main wrapper to 1310

--- src/1310.py
+++ src/1310.py
@@ -1,24 +1,29 @@
 """https://www.urionlinejudge.com.br/judge/pt/problems/view/1310"""
 
 
-while True:
-    try:
-        qty_days = int(input())
-        daily_cost = int(input())
+def main():
+    while True:
+        try:
+            qty_days = int(input())
+            daily_cost = int(input())
 
-        # Calculate the Max Subarray Sum while reading it for the first time
-        global_max = current_max = int(input()) - daily_cost
-        for i in range(1, qty_days):
-            day_receipt = int(input()) - daily_cost
-            current_max = max(day_receipt, current_max + day_receipt)
+            # Calculate the Max Subarray Sum while reading it for the first time
+            global_max = current_max = int(input()) - daily_cost
+            for i in range(1, qty_days):
+                day_receipt = int(input()) - daily_cost
+                current_max = max(day_receipt, current_max + day_receipt)
 
-            if current_max > global_max:
-                global_max = current_max
+                if current_max > global_max:
+                    global_max = current_max
 
-        # Using this if is faster than using max()
-        if 0 > global_max:
-            print('0')
-        else:
-            print(global_max)
-    except EOFError:
-        break
+            # Using this if is faster than using max()
+            if 0 > global_max:
+                print('0')
+            else:
+                print(global_max)
+        except EOFError:
+            break
+
+
+if __name__ == '__main__':
+    main()
