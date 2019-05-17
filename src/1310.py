0668039a60a5e026ae1c686f8d73720782b4b90e
João Francisco <joaofbsm@hotmail.com>
2019-05-17 00:49:18 -0300

    Optimize 1310 solution

--- src/1310.py
+++ src/1310.py
@@ -1,26 +1,24 @@
 """https://www.urionlinejudge.com.br/judge/pt/problems/view/1310"""
 
 
-def mss(receipt_vector, qty_days):
-    global_max = current_max = receipt_vector[0]
-
-    for i in range(1, qty_days):
-        current_max = max(receipt_vector[i], current_max + receipt_vector[i])
-
-        if current_max > global_max:
-            global_max = current_max
-
-    return global_max
-
-
 while True:
     try:
         qty_days = int(input())
         daily_cost = int(input())
-        receipt_vector = []
-        for _ in range(qty_days):
-            receipt_vector.append(int(input()) - daily_cost)
 
-        print(max(0, mss(receipt_vector, qty_days)))
+        # Calculate the Max Subarray Sum while reading it for the first time
+        global_max = current_max = int(input()) - daily_cost
+        for i in range(1, qty_days):
+            day_receipt = int(input()) - daily_cost
+            current_max = max(day_receipt, current_max + day_receipt)
+
+            if current_max > global_max:
+                global_max = current_max
+
+        # Using this if is faster than using max()
+        if 0 > global_max:
+            print('0')
+        else:
+            print(global_max)
     except EOFError:
         break
