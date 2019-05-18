14b21e213ca7e5b5a278da7803746437120a30a1
João Francisco <joaofbsm@hotmail.com>
2019-05-18 13:40:27 -0300

    Add dinamic program take on 2664

--- src/2664.py
+++ src/2664.py
@@ -1,20 +1,23 @@
 """https://www.urionlinejudge.com.br/judge/pt/problems/view/2664"""
 
 
-def create_exercise_program(time_remaining, current_difficulty, min_difficulty, max_difficulty):
+def create_exercise_program(memo, time_remaining, current_difficulty, min_difficulty, max_difficulty):
     # Base case
-    if time_remaining == 1:
+    if time_remaining == 0:
         return 1
 
-    qty_programs = 0
+    qty_programs = memo[time_remaining].get(current_difficulty, 0)
 
-    if current_difficulty == min_difficulty:
-        qty_programs += create_exercise_program(time_remaining - 1, current_difficulty + 1, min_difficulty, max_difficulty)
-    elif current_difficulty == max_difficulty:
-        qty_programs += create_exercise_program(time_remaining - 1, current_difficulty - 1, min_difficulty, max_difficulty)
-    else:
-        qty_programs += create_exercise_program(time_remaining - 1, current_difficulty + 1, min_difficulty, max_difficulty)
-        qty_programs += create_exercise_program(time_remaining - 1, current_difficulty - 1, min_difficulty, max_difficulty)
+    if qty_programs == 0:
+        if current_difficulty == min_difficulty:
+            qty_programs = create_exercise_program(memo, time_remaining - 1, current_difficulty + 1, min_difficulty, max_difficulty)
+        elif current_difficulty == max_difficulty:
+            qty_programs = create_exercise_program(memo, time_remaining - 1, current_difficulty - 1, min_difficulty, max_difficulty)
+        else:
+            qty_programs += create_exercise_program(memo, time_remaining - 1, current_difficulty + 1, min_difficulty, max_difficulty)
+            qty_programs += create_exercise_program(memo, time_remaining - 1, current_difficulty - 1, min_difficulty, max_difficulty)
+
+        memo[time_remaining][current_difficulty] = qty_programs
 
     return qty_programs
 
@@ -22,9 +25,13 @@ def create_exercise_program(time_remaining, current_difficulty, min_difficulty, 
 def main():
     total_time, min_difficulty, max_difficulty = [int(i) for i in input().split()]
 
+    memo = [{} for _ in range(total_time)]
     qty_programs = 0
+
+    # Adjusts total time for 0 indexing
+    total_time -= 1
     for i in range(min_difficulty, max_difficulty + 1):
-        qty_programs += create_exercise_program(total_time, i, min_difficulty, max_difficulty)
+        qty_programs += create_exercise_program(memo, total_time, i, min_difficulty, max_difficulty)
 
     print((qty_programs % (10 ** 9 + 7)))
 
