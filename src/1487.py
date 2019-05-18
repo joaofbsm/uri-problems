6f72a09f4711db38a625e00f0ad1bfc151ea6ec2
João Francisco <joaofbsm@hotmail.com>
2019-05-17 21:04:05 -0300

    Add alternative version of 1487

--- src/1487.py
+++ src/1487.py
@@ -1,18 +1,6 @@
 """https://www.urionlinejudge.com.br/judge/pt/problems/view/1487"""
 
 
-def sort_first(array):
-    return array[0]
-
-
-def choose_best_attraction(attractions, time_remaining):
-    for cost_benefit, attraction_duration, attraction_score in attractions:
-        if attraction_duration <= time_remaining:
-            return attraction_score, time_remaining - attraction_duration
-
-    return 0, time_remaining
-
-
 def main():
     instance_number = 1
     while True:
@@ -22,22 +10,31 @@ def main():
             break
         time_remaining = time_available
 
+        smallest_duration = 999999
+
         total_score = 0
         attractions = []
         for _ in range(num_attractions):
             attraction_duration, attraction_score = [int(i) for i in input().split()]
             cost_benefit = attraction_score / attraction_duration
             attractions.append((cost_benefit, attraction_duration, attraction_score))
+            if attraction_duration < smallest_duration:
+                smallest_duration = attraction_duration
 
         # Order list in decreasing order of cost_benefit
-        attractions.sort(key=sort_first, reverse=True)
+        attractions.sort(key=lambda x: x[0], reverse=True)
 
-        while True:
-            chosen_score, time_remaining = choose_best_attraction(attractions, time_remaining)
-            if chosen_score == 0:
-                break
-            else:
-                total_score += chosen_score
+        last_index = 0
+
+        while time_remaining >= smallest_duration:
+            for i in range(last_index, len(attractions)):
+                a = attractions[i]
+                if a[1] <= time_remaining:
+                    total_score += a[2]
+                    time_remaining -= a[1]
+                    last_index = i
+
+                    break
 
         print('Instancia {}\n{}\n'.format(instance_number, total_score))
 
