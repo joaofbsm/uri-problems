af7c46ed9805a6516990400606e82883c86e0ed1
João Francisco <joaofbsm@hotmail.com>
2019-05-18 16:32:50 -0300

    Add 2664.cpp 0.2s

--- src/2664.cpp
+++ src/2664.cpp
@@ -1,60 +1,58 @@
 #include <iostream>
-#include <stdio.h>
+#include <string.h>
 
 using namespace std;
 
 
-long long int create_exercise_program(int time_remaining, int current_difficulty, int min_difficulty, int max_difficulty, long long int* memo[max_difficulty + 1]) {
+long long mod = 1000000007;
+long long memo[50][100000];
+
+
+long long create_exercise_program(int time_remaining, int current_difficulty, int min_difficulty, int max_difficulty) {
+    // Border cases
+    if (current_difficulty < min_difficulty || current_difficulty > max_difficulty) {
+        return 0;
+    }
+
+    // Base case
     if (time_remaining == 0) {
         return 1;
     }
 
-    long long int qty_programs = memo[time_remaining][current_difficulty];
-
-    if (qty_programs == 0) {
-        if (current_difficulty == min_difficulty) {
-            qty_programs = create_exercise_program(time_remaining - 1, current_difficulty + 1, min_difficulty, max_difficulty, memo);
-        }
-        else if (current_difficulty == max_difficulty) {
-            qty_programs = create_exercise_program(time_remaining - 1, current_difficulty - 1, min_difficulty, max_difficulty, memo);
-        }
-        else {
-            qty_programs += create_exercise_program(time_remaining - 1, current_difficulty + 1, min_difficulty, max_difficulty, memo);
-            qty_programs += create_exercise_program(time_remaining - 1, current_difficulty - 1, min_difficulty, max_difficulty, memo);
-        }
-
-        memo[time_remaining][current_difficulty] = qty_programs;
+    // Compute memoization matrix position
+    if (memo[time_remaining][current_difficulty] == 0) {
+        memo[time_remaining][current_difficulty] += create_exercise_program(time_remaining - 1, current_difficulty + 1, min_difficulty, max_difficulty);
+        memo[time_remaining][current_difficulty] += create_exercise_program(time_remaining - 1, current_difficulty - 1, min_difficulty, max_difficulty);
+        memo[time_remaining][current_difficulty] %= mod;
     }
 
-    return qty_programs;
+    return memo[time_remaining][current_difficulty];
 }
 
 
 int main() {
 
+    // Read input
     int total_time, min_difficulty, max_difficulty;
-
     cin >> total_time >> min_difficulty >> max_difficulty;
 
+    // Adjust difficulties for sequential indexing from 0
     max_difficulty = max_difficulty - min_difficulty;
     min_difficulty = 0;
 
-    long long int memo[total_time][max_difficulty + 1] = {};
-
-    long long int qty_programs = 0;
+    // Initialize very position with 0
+    memset(memo, 0, sizeof(memo));
 
     // Adjusts total time for 0 indexing
     total_time = total_time - 1;
 
-    for (int i = 0; i < (max_difficulty + 1); i++) {
-        qty_programs += create_exercise_program(total_time, i, min_difficulty, max_difficulty, memo);
+    // Calculate the total quantity of programs by summing the quantity for each starting difficulty
+    long long qty_programs = 0;
+    for (int i = 0; i <= max_difficulty; i++) {
+        qty_programs += create_exercise_program(total_time, i, min_difficulty, max_difficulty);
     }
 
-    long long int mod = 1000000007;
-
-    cout << qty_programs % (mod);
-
-    free(memo);
+    cout << qty_programs % mod << endl;
 
     return 0;
 }
