1e5eb809128f7f58d4faa5c5cbcbfe36d9b62fa9
João Francisco <joaofbsm@hotmail.com>
2019-05-19 03:03:28 -0300

    Add 2241.py with 2-dim array

--- src/2241.py
+++ src/2241.py
@@ -5,17 +5,17 @@ def main():
     table_dim, qty_plays = [int(i) for i in input().split()]
 
     # Initialize table with all 0's
-    table = [[[0 for _ in range(table_dim)] for _ in range(table_dim)] for _ in range(table_dim)]
+    table = [[0 for _ in range(table_dim)] for _ in range(table_dim)]
 
     # Black stones
     for _ in range(qty_plays):
         i, j = [int(a) - 1 for a in input().split()]
-        table[i][j][0] = 1
+        table[i][j] = 1
 
     # White stones
     for _ in range(qty_plays):
         i, j = [int(a) - 1 for a in input().split()]
-        table[i][j][0] = 2
+        table[i][j] = 2
 
     black_points = 0
     white_points = 0
@@ -26,59 +26,59 @@ def main():
                 previous_n = n - 1
                 winner = 0
 
-                current_value = table[i][j][previous_n]
+                current_value = table[i][j]
 
                 if current_value != 0:
                     if current_value == -1:
-                        table[i][j][n] = -1
+                        table[i][j] = -1
                         continue
 
                     if winner == 0 or winner == current_value:
                         winner = current_value
                     else:
-                        table[i][j][n] = -1
+                        table[i][j] = -1
                         continue
 
-                current_value = table[i + 1][j][previous_n]
+                current_value = table[i + 1][j]
 
                 if current_value != 0:
                     if current_value == -1:
-                        table[i][j][n] = -1
+                        table[i][j] = -1
                         continue
 
                     if winner == 0 or winner == current_value:
                         winner = current_value
                     else:
-                        table[i][j][n] = -1
+                        table[i][j] = -1
                         continue
 
-                current_value = table[i][j + 1][previous_n]
+                current_value = table[i][j + 1]
 
                 if current_value != 0:
                     if current_value == -1:
-                        table[i][j][n] = -1
+                        table[i][j] = -1
                         continue
 
                     if winner == 0 or winner == current_value:
                         winner = current_value
                     else:
-                        table[i][j][n] = -1
+                        table[i][j] = -1
                         continue
 
-                current_value = table[i + 1][j + 1][previous_n]
+                current_value = table[i + 1][j + 1]
 
                 if current_value != 0:
                     if current_value == -1:
-                        table[i][j][n] = -1
+                        table[i][j] = -1
                         continue
 
                     if winner == 0 or winner == current_value:
                         winner = current_value
                     else:
-                        table[i][j][n] = -1
+                        table[i][j] = -1
                         continue
 
-                table[i][j][n] = winner
+                table[i][j] = winner
 
                 if winner == 1:
                     black_points += 1
