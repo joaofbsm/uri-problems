6a2dc1d5bb7d636b704a9c1f9be5e55fc3e3585a
Joao Martins <joaofbsm@hotmail.com>
2019-05-20 01:28:43 -0300

    Add alternative rectangle finding code for 2796.py

--- src/2796.py
+++ src/2796.py
@@ -76,6 +76,53 @@ def find_largest_table(tables, max_areas):
 
     return tables[i][1], tables[i][2]
 
+def maximal_rectangle(m, n, house_plant, smallest_table_area, smallest_table_dim):
+
+    left = [0] * n # initialize left as the leftmost boundary possible
+    right = [n] * n # initialize right as the rightmost boundary possible
+    height = [0] * n
+
+    maxarea = 0
+
+    areas = []
+
+    for i in range(m):
+        max_area = 0
+        max_height = 0
+        max_width = 0
+
+        cur_left, cur_right = 0, n
+        # update height
+        for j in range(n):
+            if house_plant[i][j] == '.': height[j] += 1
+            else: height[j] = 0
+        # update left
+        for j in range(n):
+            if house_plant[i][j] == '.': left[j] = max(left[j], cur_left)
+            else:
+                left[j] = 0
+                cur_left = j + 1
+        # update right
+        for j in range(n-1, -1, -1):
+            if house_plant[i][j] == '.': right[j] = min(right[j], cur_right)
+            else:
+                right[j] = n
+                cur_right = j
+        # update the area
+        for j in range(n):
+            if max_area < height[j] * (right[j] - left[j]) and smallest_table_area <= height[j] * (right[j] - left[j]) and smallest_table_dim <= height[j] and smallest_table_dim <= (right[j] - left[j]):
+                max_area = height[j] * (right[j] - left[j])
+                max_height = height[j]
+                max_width = (right[j] - left[j])
+                areas.append((max_area, max_height, max_width))
+
+            if maxarea < height[j] * (right[j] - left[j]):
+                maxarea = height[j] * (right[j] - left[j])
+
+    areas.sort(key=lambda x: x[0], reverse=True)
+
+    return areas
+
 
 def main():
     n, m = [int(i) for i in input().split()]
@@ -87,15 +134,32 @@ def main():
 
     tables = []
 
+    smallest_table_dim = 1000
+    smallest_table_area = 1000
+
     # Read all tables
     for i in range(int(input())):
-        c, l = [int(i) for i in input().split()]
-        tables.append((c * l, c, l))  # Add area as a component
+        length, width = [int(i) for i in input().split()]
+        tables.append((length * width, length, width))  # Add area as a component
+
+        if (length * width < smallest_table_area):
+            smallest_table_area = length * width
+        if (length < smallest_table_dim):
+            smallest_table_dim = length
+        if (width < smallest_table_dim):
+            smallest_table_dim = width
 
     # Sort tables by area and use width as tiebreaker
     tables.sort(key=lambda x: (x[0], x[2]), reverse=True)
 
+    #print(len(maximal_rectangle(n, m, house_plant)))
+    #print(maximal_rectangle(n, m, house_plant, smallest_table_area, smallest_table_dim))
+
+    #max_areas = maximal_rectangle(n, m, house_plant, smallest_table_area, smallest_table_dim)
+
     max_areas = get_max_areas(n, m, house_plant)
+
+    print(max_areas)
 
     print(*find_largest_table(tables, max_areas))
 
